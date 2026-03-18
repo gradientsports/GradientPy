f3fc797ad00bfd46dd54edaba1843c1ffac61ff4
rorybrespff <rory.breslin@pff.com>
2026-03-18 15:35:03 +0000

    Adding setup.py to support installation of GradientPy package, as well as change to function call in cross_data.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GradientPy",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.28.0",
+        "pandas>=1.5.0",
+        "numpy>=1.23.0",
+        "python-dotenv>=0.20.0",
+    ],
+    author="Gradient Sports",
+    description="Python library for accessing the Gradient Sports REST API",
+    url="https://github.com/gradientsports/GradientPy",
+)
